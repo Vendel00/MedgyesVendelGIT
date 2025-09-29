3b0366e0de71ee547f48a8357f45ea334f808b32
Vendel00 <115153559+Vendel00@users.noreply.github.com>
2025-09-29 11:21:53 +0200

    09.29 orai feladat

---
+++ FullStack/frontend/src/App.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css'; 
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Oldusers from './Oldusers.jsx';
+import Newusers from './Newusers.jsx';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+function App() {
+return (
+    <>
+    <div>
+    <BrowserRouter>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/newusers">Új felhasználó</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    
+    <Routes>
+      <Route path="/" element= {<Oldusers/>} />
+      <Route path="/newusers" element= {<Newusers/>} />
+    </Routes>
+    </BrowserRouter>
+    </div>
+  </>
+)
+}
+export default App;
